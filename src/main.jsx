0c8cf20ab49b11ca489600b0e343739e57891d7d
dya-code <entrydia7691@gmail.com>
2022-02-11 13:54:00 +0900

    i

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './Main.css'
 import App from './App'
 
 ReactDOM.render(
-  <BrowserRouter>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <App />
   </BrowserRouter>,
   document.getElementById('root')
