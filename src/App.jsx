8d54a126c5adee9526935a24d4183bd97c503a24
dya-code <entrydia7691@gmail.com>
2022-02-11 13:51:21 +0900

    h

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,12 @@ import Home from './pages/home'
 import Signin from './pages/signin'
 import List from './pages/list'
 import MyPage from './pages/mypage'
-import { HashRouter as Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
 
   return (
-    <Routes>
+    <Routes basename={process.env.PUBLIC_URL}>
       <Route path="/" element={ <Home /> } />
       <Route path="/in" element={ <Signin /> } />
       <Route path="/list" element={ <List /> } />
