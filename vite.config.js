2cfea39a05f2cb1024eef9c554d01d7e1bf14a99
dya-code <entrydia7691@gmail.com>
2022-02-11 12:48:18 +0900

    edit for hosting

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  base: './',
+  plugins: [react()],
+  build:{
+    target:['edge90','chrome90','firefox90','safari15']
+  }
 })
