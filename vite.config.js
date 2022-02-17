eed6a71d76a7c0a3ec31185d23126822979653c7
dya-code <entrydia7691@gmail.com>
2022-02-17 19:44:11 +0900

    update vercel.json

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Sekai/',
   plugins: [react()],
   build:{
     target:['edge90','chrome90','firefox90','safari15']
